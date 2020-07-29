f7f23af77e7c4532a6472b2e5486adc0bbb5fc2b
1637116722 <68009539+1637116722@users.noreply.github.com>
2020-07-29 21:31:19 +0800

    ?

---
+++ 2DRunGame/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   private void Start()
+    {
+        print(Random.value);
+        print(Mathf.PI);
+
+
+        Time.timeScale = 10;
+
+        float r=Random.Range(100.9f, 200.5f);
+        print("隨機值:" + r);
+
+        int ri = Random. Range(1,3);
+        print("隨機整數:" + ri);
+        Cursor.visible = false;
+        print("-9的绝对值:" + Mathf.Abs(-9));
+    }
+    private void Update()
+    {
+        // print("遊戲時間:" + Time.time);
+        print("玩家是否按任意键：" + Input.anyKeyDown);
+    }
+}
---
+++ 2DRunGame/Assets/Scripts/learnAPI.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class learnAPI : MonoBehaviour
+{
+    public GameObject sphere;
+
+    public Transform tra;
+
+    public Transform cube;
+
+    private void Start()
+    {
+        print(sphere.layer);
+
+        print("球的坐标：" + tra.position);
+
+        tra.localScale = new Vector3(7, 7, 7);
+    }
+
+    private void Update()
+    {
+        cube.Rotate(0, 0, 10);
+    }
+}
